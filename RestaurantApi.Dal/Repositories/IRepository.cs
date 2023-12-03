d46527f384dd85375a97b2d53f7ccb2f05b57270
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 01:49:54 +0200

    Add IRepository<T>

---
+++ RestaurantApi.Dal/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿
+namespace RestaurantApi.Dal.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Add(T entity);
+        Task Update(T entity, int id);
+        Task Delete(int id);
+        Task Save();
+
+    }
+}
