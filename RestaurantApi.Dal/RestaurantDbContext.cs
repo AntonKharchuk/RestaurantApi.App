de6b719cddf256ba5fbc9da79a9a00ba8d4ecfc1
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 01:51:16 +0200

    Add db context class

---
+++ RestaurantApi.Dal/RestaurantDbContext.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using RestaurantApi.Dal.Models;
+
+namespace RestaurantApi.Dal
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Portion> Portions { get; set; }
+        public DbSet<PriceListItem> PriceList { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public RestaurantDbContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PriceListItem>().ToTable("PriceList");
+
+            modelBuilder.Entity<Meal>()
+              .HasMany(m => m.Ingredients)
+              .WithMany(i => i.Meals)
+              .UsingEntity(j => j.ToTable("MealIngredients"));
+        }
+    }
+}
