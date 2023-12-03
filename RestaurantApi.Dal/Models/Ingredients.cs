fbc8750e207cd9c53b54e50e1a2b2cbd2f2d1792
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 01:45:29 +0200

    Add models

---
+++ RestaurantApi.Dal/Models/Ingredients.cs
@@ -0,0 +1,52 @@
+﻿
+namespace RestaurantApi.Dal.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<Meal>? Meals { get; set; }
+
+    }
+    public class Meal
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public List<Ingredient>? Ingredients { get; set; }
+    }
+
+    public class Portion
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+    public class PriceListItem
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal Amount { get; set; }
+        public Meal? Meal { get; set; }
+        public int MealId { get; set; }
+        public int PortionId { get; set; }
+        public Portion? Portion { get; set; }
+    }
+    public class Order
+    {
+        public int Id { get; set; }
+        public string? CustomerName { get; set; }
+        public DateTime Date { get; set; }
+    }
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public string? Coment { get; set; }
+        public bool IsReady { get; set; }
+        public Order Order { get; set; } = null!;
+        public int OrderId { get; set; }
+        public PriceListItem? PriceListItem;
+        public int PriceListItemId { get; set; }
+
+    }
+
+}
