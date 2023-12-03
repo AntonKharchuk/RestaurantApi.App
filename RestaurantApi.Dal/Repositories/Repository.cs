62087ef55e38a72fbd13d964f84f7fd6a9f51891
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 01:59:11 +0200

    implement IRepository generic

---
+++ RestaurantApi.Dal/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantApi.Dal.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly RestaurantDbContext _context;
+
+        private DbSet<T> _table;
+
+        public Repository(RestaurantDbContext context)
+        {
+            _context = context;
+            _table = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _table.ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            var entity = await _table.FindAsync(id);
+            ThrowIfNull(entity);
+            return entity!;
+        }
+
+        public async Task Add(T entity)
+        {
+            ThrowIfNull(entity);
+            await _table.AddAsync(entity);
+        }
+
+        public async Task Update(T entity, int id)
+        {
+            var tableEntity = await GetById(id);
+            _context.Entry(tableEntity).CurrentValues.SetValues(entity);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await _table.FindAsync(id);
+            ThrowIfNull(entity);
+            _table.Remove(entity!);
+        }
+        private static void ThrowIfNull(T? entity)
+        {
+            if (entity is null)
+            {
+                throw new ArgumentNullException(nameof(entity), "Entity with the specified ID not found.");
+            }
+        }
+    }
+}
