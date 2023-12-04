319bc41d2341f7707fbc3e219a75836b9ec226fa
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 02:03:08 +0200

    Set all Interfaces

---
+++ RestaurantApi.Dal/Services/IMealCRUDService.cs
@@ -0,0 +1,11 @@
+﻿
+using RestaurantApi.Dal.Models;
+
+namespace RestaurantApi.Dal.Services
+{
+    public interface IMealCRUDService: ICRUDService<Meal>
+    {
+        Task<IEnumerable<Meal>> GetItemsInRange(int startId, int endId);
+
+    }
+}
---
+++ RestaurantApi.Dal/Services/IOrderItemCRUDService.cs
@@ -0,0 +1,10 @@
+﻿
+using RestaurantApi.Dal.Models;
+
+namespace RestaurantApi.Dal.Services
+{
+    public interface IOrderItemCRUDService:ICRUDService<OrderItem>
+    {
+        Task<IEnumerable<OrderItem>> GetbyOrderId(int id);
+    }
+}
