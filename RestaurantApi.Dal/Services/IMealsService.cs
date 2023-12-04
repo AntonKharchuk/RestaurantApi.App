2245c63b581b59da393485cd8932447fd2e02414
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 02:41:27 +0200

    Add background services for controllers to DAL

---
+++ RestaurantApi.Dal/Services/IMealsService.cs
@@ -0,0 +1,19 @@
+﻿
+using RestaurantApi.Dal.Models;
+
+namespace RestaurantApi.Dal.Services
+{
+    public interface IMealsService
+    {
+        ICRUDService<Ingredient> IngredientService { get; }
+        IMealCRUDService MealService { get; }
+        ICRUDService<Portion> PortionService { get; }
+        ICRUDService<PriceListItem> PriceListService { get; }
+    }
+    public interface IOrdersService
+    {
+        ICRUDService<PriceListItem> PriceListService { get; }
+        ICRUDService<Order> OrderService { get; }
+        IOrderItemCRUDService OrderItemService { get; }
+    }
+}
