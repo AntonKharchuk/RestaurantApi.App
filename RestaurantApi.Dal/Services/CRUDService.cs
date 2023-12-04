07fa0f0de52b1ab1ea662415f649bc81939027d5
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 02:07:11 +0200

    implement class CRUDService<T>

---
+++ RestaurantApi.Dal/Services/CRUDService.cs
@@ -0,0 +1,43 @@
+﻿
+using RestaurantApi.Dal.Repositories;
+
+namespace RestaurantApi.Dal.Services
+{
+    public class CRUDService<T> : ICRUDService<T> where T : class
+    {
+        private readonly IRepository<T> _repository;
+
+        public CRUDService(IRepository<T> repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public async Task<IEnumerable<T>> GetAllItems()
+        {
+            return await _repository.GetAll();
+        }
+
+        public async Task<T> GetItemById(int id)
+        {
+            return await _repository.GetById(id);
+        }
+
+        public async Task CreateItem(T entity)
+        {
+            await _repository.Add(entity);
+            await _repository.Save();
+        }
+
+        public async Task UpdateItem(T entity, int id)
+        {
+            await _repository.Update(entity, id);
+            await _repository.Save();
+        }
+
+        public async Task DeleteItem(int id)
+        {
+            await _repository.Delete(id);
+            await _repository.Save();
+        }
+    }
+}
