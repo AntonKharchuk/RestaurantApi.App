fdb22140661d59d256f4dba886b8cf455f23b8a8
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 02:00:02 +0200

    Add interface ICRUDService<T> where T : class

---
+++ RestaurantApi.Dal/Services/ICRUDService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RestaurantApi.Dal.Services
+{
+    public interface ICRUDService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllItems();
+        Task<T> GetItemById(int id);
+        Task CreateItem(T entity);
+        Task UpdateItem(T entity, int id);
+        Task DeleteItem(int id);
+    }
+}
