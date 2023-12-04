cc6d282b0dba4fa39b6b356ad944b2a2dea98a0d
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-04 02:30:55 +0200

    Add  RestaurantDbContext, DI  and data to db to program

--- RestaurantApi.App/Program.cs
+++ RestaurantApi.App/Program.cs
@@ -1,8 +1,20 @@
+using RestaurantApi.App.RestaurantApi.App;
+using RestaurantApi.Dal;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var dbContesxt = new RestaurantDbContext();
+
+builder.Services.AddSingleton(dbContesxt);
+
+var restaurantDI = new RestaurantDI();
+restaurantDI.ConfigureServices(builder.Services);
+restaurantDI.AddDataToDB(dbContesxt);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
