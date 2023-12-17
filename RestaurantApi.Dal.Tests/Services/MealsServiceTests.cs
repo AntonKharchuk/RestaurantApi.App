dc04c14399206f9355f118eaa5079689481896d3
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-17 19:13:32 +0200

    Add tests to cover MealsService and OrdersService

---
+++ RestaurantApi.Dal.Tests/Services/MealsServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using Moq; // You may need to install the Moq NuGet package for mocking
+using RestaurantApi.Dal.Services;
+using RestaurantApi.Dal.Models;
+
+public class MealsServiceTests
+{
+    [Fact]
+    public void Constructor_InitializesProperties()
+    {
+        // Arrange
+        var ingredientServiceMock = new Mock<ICRUDService<Ingredient>>();
+        var mealServiceMock = new Mock<IMealCRUDService>();
+        var portionServiceMock = new Mock<ICRUDService<Portion>>();
+        var priceListServiceMock = new Mock<ICRUDService<PriceListItem>>();
+
+        // Act
+        var mealsService = new MealsService(
+            ingredientServiceMock.Object,
+            mealServiceMock.Object,
+            portionServiceMock.Object,
+            priceListServiceMock.Object
+        );
+
+        // Assert
+        Assert.NotNull(mealsService.IngredientService);
+        Assert.NotNull(mealsService.MealService);
+        Assert.NotNull(mealsService.PortionService);
+        Assert.NotNull(mealsService.PriceListService);
+    }
+
+}
