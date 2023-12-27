4cb93cd4ff5a6c89ddd9d4184ad0d0f8f8ab2aa9
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-12-27 13:34:17 +0200

    move class code form file to spesific file

---
+++ RestaurantApi.Dal/Services/OrdersService.cs
@@ -0,0 +1,20 @@
+﻿
+using RestaurantApi.Dal.Models;
+
+namespace RestaurantApi.Dal.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        
+        public ICRUDService<Order> OrderService { get; set; }
+        public IOrderItemCRUDService OrderItemService { get; set; }
+
+        public OrdersService(ICRUDService<Order> orderService,
+                            IOrderItemCRUDService orderItemService
+            )
+        {
+            OrderService = orderService;
+            OrderItemService = orderItemService;
+        }
+    }
+}
