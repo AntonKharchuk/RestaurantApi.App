b7d8afdaead763fe5a355d663ebbbacd80972ba3
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-01-03 18:46:40 +0200

    Add Ingredients controller separetely

---
+++ RestaurantApi.App/Controllers/IngrediensController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantApi.App.Models;
+using RestaurantApi.Business.Services;
+
+namespace RestaurantApi.App.Controllers
+{
+    [Route("api/Meal/[controller]")]
+    [ApiController]
+    public class IngrediensController : ControllerBase
+    {
+        private ICRUDService<Dal.Models.Ingredient> _ingredientsServise;
+
+        public IngrediensController(ICRUDService<Dal.Models.Ingredient> ingredientsServise)
+        {
+            _ingredientsServise = ingredientsServise;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Ingredient>> GetAllIngredients()
+        {
+            var asyncResult = _ingredientsServise.GetAllItemsAsync();
+
+            var result = new List<Ingredient>();
+
+            foreach (var item in await asyncResult)
+            {
+                result.Add(Parser.IngredientFromDALToApp(item));
+            }
+
+            return result;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ingredient>> GetIngredientById(int id)
+        {
+            var ingredient = await _ingredientsServise.GetItemByIdAsync(id);
+            if (ingredient is null)
+                return NotFound();
+
+            return Parser.IngredientFromDALToApp(ingredient);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Ingredient>> CreateIngredient(Ingredient ingredient)
+        {
+            await _ingredientsServise.CreateItemAsync(Parser.IngredientFromAppToDAL(ingredient));
+
+            return CreatedAtRoute("CreateIngredient", new { id = ingredient.Id }, ingredient);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateIngredient([FromQuery] int id, [FromBody] Ingredient ingredient)
+        {
+            await _ingredientsServise.UpdateItemAsync(Parser.IngredientFromAppToDAL(ingredient), id);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIngredient(int id)
+        {
+            await _ingredientsServise.DeleteItemAsync(id);
+
+            return Ok();
+        }
+
+    }
+}
